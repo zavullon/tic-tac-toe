050b64bbc89398fefa0cb65bac223cabf5337e76
Grigory <MrGriLLla@gmail.com>
2019-10-29 14:36:35 +0300

    bugfix

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import logging
-import ChatType
+from ChatType import ChatType
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -12,9 +12,8 @@ logger = logging.getLogger(__name__)
 def start(update, context):
     group_members_amount = context.bot.get_chat_members_count(update.message.chat_id)
     if group_members_amount <= 3:
-        update.message.reply_text('alo')
         chat_type = ChatType(group_members_amount - 2)
-        update.message.reply_text(str(chat_type.value))
+        update.message.reply_text(chat_type.value)
     else:
         update.message.reply_text('you can only play with 1 friend, which can be the bot itself by the way')
 
